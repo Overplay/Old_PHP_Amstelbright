285d43fdefce02d7527cc7e7beda267c02de07f6
Mitch Kahn <mitch@appdelegates.com>
2016-03-16 16:05:19 -0700

    Minor crawler placement

--- public/opp/io.overplay.mainframe/app/tv/app/OPTVOSService/OPTVOSServicePHP.js
+++ public/opp/io.overplay.mainframe/app/tv/app/OPTVOSService/OPTVOSServicePHP.js
@@ -238,7 +238,7 @@ app.factory( 'osService', [ '$log', '$http', '$rootScope', '$window', function (
         switch ( slot ) {
 
             case 0:
-                rval.top = Math.floor( 0.90 * service.windowDimension.height ) + nudge.top + 'px';
+                rval.top = Math.floor( 0.91 * service.windowDimension.height ) + nudge.top + 'px';
                 break;
 
             case 1:
