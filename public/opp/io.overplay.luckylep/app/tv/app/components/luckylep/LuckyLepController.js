7e3bfa11d0fbf2cb0c24a0d9cb995ed5869056bb
Mitch Kahn <mitch@appdelegates.com>
2016-03-16 19:47:00 -0700

    Lucky lep

--- public/opp/io.overplay.luckylep/app/tv/app/components/luckylep/LuckyLepController.js
+++ public/opp/io.overplay.luckylep/app/tv/app/components/luckylep/LuckyLepController.js
@@ -6,7 +6,35 @@ app.controller( "luckyLepController",
     function ( $scope, $timeout, $http, $interval, optvModel, $log, $window ) {
 
 
-        $scope.lep = { winner: "treb", show: true };
+        $scope.lep = { winner: "treb", show: false };
+        $scope.guests = []
+
+        function getGuests(){
+
+            $http.get("http://162.243.133.57:1337/api/v2/guest")
+                .then( function(data){
+
+                    $scope.guests = data.data;
+
+                }, function(err){
+
+                })
+        }
+
+        function pickVictim(){
+
+            if ($scope.guests.length){
+
+                var rn = Math.floor( Math.random() * ($scope.guests.length) );
+                $scope.lep.winner = $scope.guests[rn].firstName;
+                $scope.lep.show = true;
+                getGuests();
+                $timeout(function(){
+                    $scope.lep.show = false;
+                }, 30*1000);
+            }
+
+        }
         
         function modelUpdate( data ) {
            
@@ -29,5 +57,8 @@ app.controller( "luckyLepController",
         }
 
         updateFromRemote();
+
+        $interval(pickVictim, 3*60*1000)
+        getGuests()
 
     } );
